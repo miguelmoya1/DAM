51e39e09581e92880812353142921b06e14d2402
Miguelmo <miguelmoyaortega@gmail.com>
2017-10-04 20:19:46 +0200

    NOP

--- ProgramacionDeServiciosYProcesos/Ev1/Ejemplos/Java8/src/java8/Java8.java
+++ ProgramacionDeServiciosYProcesos/Ev1/Ejemplos/Java8/src/java8/Java8.java
@@ -1,4 +1,4 @@
-/***
+/** *
  * @Author: Miguel Moya Ortega
  */
 package java8;
@@ -10,11 +10,10 @@ public class Java8 {
 
     public static void main(String[] args) {
         List<Integer> lista = Arrays.asList(100, 10, 2, 6, 54, 32, 67, 12, 7);
-        
+
         lista.forEach(i -> System.out.println(i));
-        
+
         lista.stream().filter(x -> x > 7).map(Integer::reverse).forEach(System.out::println);
-        
+
     }
-    
 }
