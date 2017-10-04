d60299c950dbb42dca16afcc96532800f22a6ef2
Miguelmo <miguelmoyaortega@gmail.com>
2017-10-04 20:00:33 +0200

    no funciona

--- ProgramacionDeServiciosYProcesos/Ev1/Ejemplos/Java8/src/java8/Java8.java
+++ ProgramacionDeServiciosYProcesos/Ev1/Ejemplos/Java8/src/java8/Java8.java
@@ -11,6 +11,8 @@ public class Java8 {
     public static void main(String[] args) {
         List<Integer> lista = Arrays.asList(100, 10, 2, 6, 54, 32, 67, 12, 7);
         
+        lista.forEach(i -> System.out.println(i));
+        
         lista.stream().filter(x -> x > 7).map(Integer::reverse).forEach(System.out::println);
         
     }
