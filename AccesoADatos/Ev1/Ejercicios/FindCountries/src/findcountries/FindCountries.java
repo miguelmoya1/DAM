2c9e9f96a5edc9d19e2db000cec37d1d704d7419
Miguelmo <miguelmoyaortega@gmail.com>
2017-10-10 18:20:43 +0200

    .

---
+++ AccesoADatos/Ev1/Ejercicios/FindCountries/src/findcountries/FindCountries.java
@@ -0,0 +1,54 @@
+package findcountries;
+
+import java.io.File;
+import java.util.Scanner;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+
+/**
+ *
+ * @author Miguel Moya Ortega
+ */
+public class FindCountries {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String toFind;
+        System.out.println("Escribe el nombre: ");
+        toFind = scanner.nextLine();
+        while (!toFind.equals("")) {
+            try {
+                File inputFile = new File("countries.xml");
+                DocumentBuilderFactory dbFactory
+                        = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(inputFile);
+                doc.getDocumentElement().normalize();
+
+                NodeList nList = doc.getElementsByTagName("country");
+
+                for (int temp = 0; temp < nList.getLength(); temp++) {
+                    Node nNode = nList.item(temp);
+                    if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                        Element eElement = (Element) nNode;
+                        if (eElement.getAttribute("translations").toLowerCase()
+                                .contains(toFind.toLowerCase())) {
+                            System.out.println(eElement.getAttribute("name"));
+                        }
+                    }
+                }
+                toFind = scanner.nextLine();
+            } catch (Exception e) {
+                System.out.println("No va");
+            }
+
+        }
+
+    }
+
+}
