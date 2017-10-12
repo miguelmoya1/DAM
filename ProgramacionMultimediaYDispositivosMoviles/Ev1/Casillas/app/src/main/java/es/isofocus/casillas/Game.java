debc5a6a7ed2fa290e9ba458fdb6ab48fa9dd775
miguelmoya1 <miguelmoyaortega@gmail.com>
2017-10-12 16:57:01 +0200

    practica1

--- ProgramacionMultimediaYDispositivosMoviles/Ev1/Casillas/app/src/main/java/es/isofocus/casillas/Game.java
+++ ProgramacionMultimediaYDispositivosMoviles/Ev1/Casillas/app/src/main/java/es/isofocus/casillas/Game.java
@@ -6,8 +6,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class Game extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,19 @@ public class Game extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Bundle bundle = getIntent().getBundleExtra("data");
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        String text = "Número de elementos en la X: " + bundle.get("seekX") +
+                "\n Número de elementos en la Y: " + bundle.getInt("seekY") +
+                "\n Número de plops: " + bundle.get("seekPlots") +
+                "\n Typo: " + bundle.get("type") +
+                "\n Sonido activo: " + bundle.get("sound") +
+                "\n Vibración activada: " + bundle.get("vibration");
+
+        textView.setText(text);
     }
 
 }
--- ProgramacionMultimediaYDispositivosMoviles/Ev1/Casillas/app/src/main/java/es/isofocus/casillas/MainActivity.java
+++ ProgramacionMultimediaYDispositivosMoviles/Ev1/Casillas/app/src/main/java/es/isofocus/casillas/MainActivity.java
@@ -1,5 +1,6 @@
 package es.isofocus.casillas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonPlay;
+    SeekBar seekX, seekY, seekPlots;
+    CheckBox sound, vibration;
+    RadioButton radioButtonC, radioButtonN;
+
+    String typeButtonRadio = "colores";
+
+    int x = 3, y = 3, p = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,23 +32,70 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        buttonPlay = (Button) findViewById(R.id.buttonPlay);
+        seekX = (SeekBar)findViewById(R.id.seekX);
+        seekY = (SeekBar)findViewById(R.id.seekY);
+        seekPlots = (SeekBar)findViewById(R.id.seekPlots);
+        radioButtonC = (RadioButton) findViewById(R.id.radioButtonColors);
+        radioButtonN = (RadioButton) findViewById(R.id.radioButtonNumbers);
+
+        sound = (CheckBox) findViewById(R.id.checkBoxSound);
+        vibration = (CheckBox) findViewById(R.id.checkBoxVibration);
+
+        seekX.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onStartTrackingTouch(SeekBar bar){ }
+            public void onProgressChanged(SeekBar bar, int paramInt, boolean paramBoolean) { }
+            public void onStopTrackingTouch(SeekBar bar) { x = bar.getProgress(); }
+        });
+        seekY.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onStartTrackingTouch(SeekBar bar){ }
+            public void onProgressChanged(SeekBar bar, int paramInt, boolean paramBoolean) { }
+            public void onStopTrackingTouch(SeekBar bar) { y = bar.getProgress(); }
+        });
+        seekPlots.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onStartTrackingTouch(SeekBar bar){ }
+            public void onProgressChanged(SeekBar bar, int paramInt, boolean paramBoolean) { }
+            public void onStopTrackingTouch(SeekBar bar) { p = bar.getProgress(); }
+        });
+
+        buttonPlay.setOnClickListener(this);
+        radioButtonC.setOnClickListener(this);
+        radioButtonN.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.buttonPlay) {
+            Intent intent = new Intent(this, Game.class);
+            Bundle bundle = new Bundle();
+
+            bundle.putInt("seekX", x);
+            bundle.putInt("seekY", y);
+            bundle.putInt("seekPlots", p);
+            bundle.putString("type", typeButtonRadio);
+            bundle.putBoolean("sound", sound.isChecked());
+            bundle.putBoolean("vibration", vibration.isChecked() );
+
+            intent.putExtra("data", bundle);
+            startActivity(intent);
+        } else if (v.getId() == R.id.radioButtonColors) {
+            typeButtonRadio = "colores";
+        } else if(v.getId() == R.id.radioButtonNumbers) {
+            typeButtonRadio = "numeros";
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
