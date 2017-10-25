0949822aef2c7e9d72c69896fefef45cec652abe
Miguelmo <miguelmoyaortega@gmail.com>
2017-10-25 16:00:25 +0200

    creacion ejercicio 2

--- DesarrolloDeInterfaces/Ev1/Ejercicios/Ejercicio2/Ejercicio2/Form1.Designer.cs
+++ DesarrolloDeInterfaces/Ev1/Ejercicios/Ejercicio2/Ejercicio2/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Moya_Miguel_Ejercicio1 {
+﻿namespace Ejercicio2 {
     partial class Form1 {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -24,46 +24,35 @@
         /// </summary>
         private void InitializeComponent() {
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Location = new System.Drawing.Point(12, 13);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(259, 20);
+            this.textBox1.Size = new System.Drawing.Size(260, 20);
             this.textBox1.TabIndex = 0;
-            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
-            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
-            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
-            this.textBox1.Validated += new System.EventHandler(this.textBox1_Validated);
             // 
-            // textBox2
+            // button1
             // 
-            this.textBox2.Location = new System.Drawing.Point(13, 39);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(259, 20);
-            this.textBox2.TabIndex = 1;
-            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
-            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
-            this.textBox2.Validating += new System.ComponentModel.CancelEventHandler(this.textBox2_Validating);
-            this.textBox2.Validated += new System.EventHandler(this.textBox2_Validated);
+            this.button1.Location = new System.Drawing.Point(12, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Otro formulario";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.Shown += new System.EventHandler(this.Form1_Shown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,7 +61,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- DesarrolloDeInterfaces/Ev1/Ejercicios/Moya_Miguel_Ejercicio1/Moya_Miguel_Ejercicio1/Form1.cs
+++ DesarrolloDeInterfaces/Ev1/Ejercicios/Moya_Miguel_Ejercicio1/Moya_Miguel_Ejercicio1/Form1.cs
@@ -10,20 +10,30 @@ using System.Windows.Forms;
 
 namespace Moya_Miguel_Ejercicio1 {
     public partial class Form1 : Form {
-        public Form1() {
-            InitializeComponent();
-        }
+        public Form1() => InitializeComponent();
 
-        private void Form1_Load(object sender, EventArgs e) {
-            MessageBox.Show("Load event.");
-        }
+        private void Form1_Load(object sender, EventArgs e) => MessageBox.Show("Load event.");
 
-        private void Form1_Shown(object sender, EventArgs e) {
-            MessageBox.Show("Shown event.");
-        }
+        private void Form1_Shown(object sender, EventArgs e) => MessageBox.Show("Shown event.");
 
-        private void Form1_Closing(object sender, CancelEventArgs e) {
-            MessageBox.Show("Shown event.");
-        }
+        private void Form1_Closing(object sender, CancelEventArgs e) => MessageBox.Show("Closing event.");
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) => MessageBox.Show("Closed event.");
+
+        private void textBox1_Enter(object sender, EventArgs e) => MessageBox.Show("Enter textbox1 event.");
+
+        private void textBox2_Enter(object sender, EventArgs e) => MessageBox.Show("Enter textbox2 event.");
+
+        private void textBox1_Validated(object sender, EventArgs e) => MessageBox.Show("Validated textbox1 event.");
+
+        private void textBox2_Validated(object sender, EventArgs e) => MessageBox.Show("Validated textbox2 event.");
+
+        private void textBox1_Validating(object sender, CancelEventArgs e) => MessageBox.Show("Validating textbox1 event.");
+
+        private void textBox2_Validating(object sender, CancelEventArgs e) => MessageBox.Show("Validating textbox2 event.");
+
+        private void textBox2_Leave(object sender, EventArgs e) => MessageBox.Show("Leave textbox2 event.");
+
+        private void textBox1_Leave(object sender, EventArgs e) => MessageBox.Show("Leave textbox1 event.");
     }
 }
