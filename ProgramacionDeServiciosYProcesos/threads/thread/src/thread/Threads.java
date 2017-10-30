9cb11e5d4b8ef6932d5dfb9d93b02258344f728c
Miguelmo <miguelmoyaortega@gmail.com>
2017-10-30 16:24:14 +0100

    thread

---
+++ ProgramacionDeServiciosYProcesos/threads/thread/src/thread/Threads.java
@@ -0,0 +1,17 @@
+package thread;
+
+/**
+ *
+ * @author Miguel Moya Ortega
+ */
+public class Threads extends Thread{
+
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                
+            }
+        });
+    }
+    
+}
